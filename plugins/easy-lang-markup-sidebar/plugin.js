8d50e6167b6fdc10a10b71f01acc2f2819a48574
brichwin <brichwin@iu.edu>
2025-04-18 14:11:00 -0400

    Update plugin.js

--- plugins/easy-lang-markup-sidebar/plugin.js
+++ plugins/easy-lang-markup-sidebar/plugin.js
@@ -1,6 +1,27 @@
-/**
+/*
+ * easy-lang-markup-sidebar
  * Copyright (C) 2018 The Trustees of Indiana University
- * SPDX-License-Identifier: BSD-3-Clause
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * @file easy-lang-markup-sidebar/plugin.js
+ * @description TinyMCE plug-in which provides controls for marking up the human language of text passages.
+ * @author Brian Richwine <brichwin@iu.edu>
+ * @copyright Copyright (c) 2018 The Trustees of Indiana University
+ * SPDX-License-Identifier: GPL-3.0-only
  */
 
 /* global alert, confirm, tinyMCE */
